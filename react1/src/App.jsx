adcc3060bebb56f42539b84174690745de7d07be
Aidaleon <aidaleon@hotmail.es>
2026-01-14 13:33:19 +0100

    ejercicio 2

--- react1/src/App.jsx
+++ react1/src/App.jsx
@@ -7,42 +7,29 @@ import {Profile} from "./components/Profile.jsx";
 import {Skill} from "./components/Skill.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
+const [show, setShow] = useState(false);
+
+    const skills = ["Javascript", "jQuery", "Vue", "Angular", "React", "Node.js"]
+
+function changeShow() {
+    setShow(!show);
+}
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
         <Header/>
 
         <Profile name="Aida León Vila" age={30} profession="Web Developer"/>
 
-        <ul>
-            <Skill text="Java"/>
-            <Skill text="HTML"/>
-            <Skill text="JavaScript"/>
-            <Skill text="React"/>
-            <Skill text="SpringBoot"/>
-            <Skill text="Boostrap"/>
-        </ul>
+        <button onClick={changeShow}>Skills</button>
+
+        {show &&
+        (<ul>
+            {skills.map((skill , index) => (
+                <Skill key={index} text={skill} />
+            ))}
+        </ul>)
+        }
 
     </>
   )
